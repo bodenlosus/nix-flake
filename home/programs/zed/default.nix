425b0a0082ed143895104464dd09d719a370b846
Johannes <johannes.schmidt@outlook.de>
2025-04-20 13:05:54 +0200

    j

--- home/programs/zed/default.nix
+++ home/programs/zed/default.nix
@@ -1,93 +1,98 @@
 { config, pkgs, ... }: {
   home.packages = with pkgs; [ zed-editor-fhs ];
   xdg.configFile."zed/default-settings.json".text = ''
-      {
-        "auto_install_extensions": {
-          "biome": true,
-          "catppuccin-icons": true,
-          "charmed-icons": true,
-          "colored-zed-icons-theme": true,
-          "graphql": true,
-          "html": true,
-          "jetbrains-new-ui-icons": true,
-          "material-icon-theme": true,
-          "nix": true,
-          "nordic-nvim-theme": true,
-          "nordic-theme": true,
-          "oxocarbon": true,
-          "rose-pine-theme": true,
-          "svelte": true,
-          "vscode-icons": true
+    {
+      "assistant": {
+        "default_model": {
+          "provider": "deepseek",
+          "model": "deepseek-chat"
         },
-
-        "language_models": {
-          "deepseek": {
-            "api_url": "https://api.deepseek.com",
-            "available_models": [
-              {
-                "name": "deepseek-chat",
-                "display_name": "DeepSeek Chat",
-                "max_tokens": 64000
-              },
-              {
-                "name": "deepseek-reasoner",
-                "display_name": "DeepSeek Reasoner",
-                "max_tokens": 64000,
-                "max_output_tokens": 4096
-              }
-            ]
-          }
-        },
-
-        "autosave": "on_focus_change",
-        "session": {
-          "restore_unsaved_buffers": true
-        },
-        "features": {
-          "edit_prediction_provider": "zed"
-        },
-        "ui_font_size": 12,
-        "buffer_font_size": 12,
-        "theme": {
-          "mode": "system",
-          "light": "Rosé Pine",
-          "dark": "Rosé Pine"
-        },
-        "outline_panel": {
-          "dock": "right",
-          "scrollbar": { "show": "never" }
-        },
-        "project_panel": {
-          "dock": "right",
-          "scrollbar": { "show": "never" }
-        },
-        "scrollbar": { "show": "never" },
-        "icon_theme": "Charmed Icons",
-        "vim_mode": true,
-        "ui_font_family": "JetBrainsMono Nerd Font Propo",
-        "buffer_font_family": "JetBrainsMono Nerd Font Mono",
-        "git": {
-          "git_gutter": "tracked_files",
-          "inline_blame": { "enabled": true, "show_commit_summary": true },
-          "hunk_style": null
-        },
-        "git_panel": { "dock": "left" },
-        "terminal": {
-          "dock": "bottom"
-        },
-        "toolbar": {
-          "breadcrumbs": true,
-          "quick_actions": true,
-          "selections_menu": true
-        },
-        "gutter": {
-          "folds": true,
-          "line_numbers": true,
-          "code_actions": true
-        },
-        "edit_predictions": {
-          "mode": "subtle"
+        "version": "2"
+      },
+      "auto_install_extensions": {
+        "biome": true,
+        "catppuccin-icons": true,
+        "charmed-icons": true,
+        "colored-zed-icons-theme": true,
+        "graphql": true,
+        "html": true,
+        "jetbrains-new-ui-icons": true,
+        "material-icon-theme": true,
+        "nix": true,
+        "nordic-nvim-theme": true,
+        "nordic-theme": true,
+        "oxocarbon": true,
+        "rose-pine-theme": true,
+        "svelte": true,
+        "vscode-icons": true
+      },
+      "language_models": {
+        "deepseek": {
+          "api_url": "https://api.deepseek.com",
+          "available_models": [
+            {
+              "name": "deepseek-chat",
+              "display_name": "DeepSeek Chat",
+              "max_tokens": 64000
+            },
+            {
+              "name": "deepseek-reasoner",
+              "display_name": "DeepSeek Reasoner",
+              "max_tokens": 64000,
+              "max_output_tokens": 4096
+            }
+          ]
         }
+      },
+
+      "autosave": "on_focus_change",
+      "session": {
+        "restore_unsaved_buffers": true
+      },
+      "features": {
+        "edit_prediction_provider": "zed"
+      },
+      "ui_font_size": 14,
+      "buffer_font_size": 16,
+      "theme": {
+        "mode": "system",
+        "light": "Github Dark",
+        "dark": "Rosé Pine"
+      },
+      "outline_panel": {
+        "dock": "right",
+        "scrollbar": { "show": "never" }
+      },
+      "project_panel": {
+        "dock": "right",
+        "scrollbar": { "show": "never" }
+      },
+      "scrollbar": { "show": "never" },
+      "icon_theme": "Charmed Icons",
+      "vim_mode": true,
+      "ui_font_family": "JetBrainsMono Nerd Font Propo",
+      "buffer_font_family": "JetBrainsMono Nerd Font Mono",
+      "git": {
+        "git_gutter": "tracked_files",
+        "inline_blame": { "enabled": true, "show_commit_summary": true },
+        "hunk_style": null
+      },
+      "git_panel": { "dock": "left" },
+      "terminal": {
+        "dock": "bottom"
+      },
+      "toolbar": {
+        "breadcrumbs": true,
+        "quick_actions": true,
+        "selections_menu": true
+      },
+      "gutter": {
+        "folds": true,
+        "line_numbers": true,
+        "code_actions": true
+      },
+      "edit_predictions": {
+        "mode": "subtle"
       }
     }
 
