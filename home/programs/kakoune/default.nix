fbb28951163179e8383dc6fb99db5234c688496f
Johannes <johannes.schmidt@outlook.de>
2025-10-27 10:25:08 +0100

    a

--- home/programs/kakoune/default.nix
+++ home/programs/kakoune/default.nix
@@ -3,11 +3,11 @@ let
   utils = import ./cfg.nix { inherit pkgs; };
   kakoune = utils.kakouneConfig {
     config = "";
-    plugins = with pkgs; [
-      kakoune-lsp
+    plugins = with pkgs.kakounePlugins; [
+      ka
     ];
   };
 in
 {
-  home.packages = [ kakoune ];
+  home.packages = [ kakoune pkgs.kak-lsp pkgs.kak-tree-sitter];
 }
